4d428e1b983394d5f44dc45a4402cf849264428d
Lucas Alves da Conceição <lcsalves1999@gmail.com>
2020-08-30 01:49:20 -0300

    Introduzindo JS no código HTML

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Declarando as variáveis
+var nome = "Lucas Alves"
+var n1 = 21
+var n2 = 10
+var frase = "Alemanha é o melhor time do mundo!" //String de exemplo.
+
+//alert("Bem-vindo " + nome +"!")
+//alert(nome + " tem " + idade + " anos")
+console.log(nome)
+console.log(n1)
+console.log(n1 * n2) //Operação aritmética com números
+
+alert(frase.replace("Alemanha", "Brasil")) //Troca a palavra selecionada.
+
+console.log(frase.toLowerCase()) //Todo o texto fica em minúsculo.
+console.log(frase.toUpperCase()) //Todo o texto fica em maiúsculo.
